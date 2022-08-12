247d7904e3e01c744f5fb8bfca15df7822bb3ff2
rmac0411 <rmac0411@gmail.com>
2022-08-12 10:57:25 -0700

    form data is getting through

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
-//Controllers Route
+//Controllers Route 
 app.use('/places', require('./controllers/places'))
 
 //Homepage Route
