7d62230c3a3d5bb10956ab660d89e43b417dd857
rmac0411 <rmac0411@gmail.com>
2022-08-18 19:19:49 -0700

    style

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,7 +5,24 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{ data.place.name }</h1>
+                <h1>{data.place.name}</h1>
+                <p>Rating:</p>
+                <img src={data.place.pic} alt={data.place.name}></img>
+                <h2>Info</h2>
+                <p>Location: {data.place.city}, {data.place.state}
+                <br></br>
+                Cuisine: {data.place.cuisines}
+                </p>
+                <h2>Comments</h2>
+                <p>No comments yet!</p>
+                <a href="" className="btn btn-warning">
+                    Edit
+                </a>
+                <form>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
             </main>
         </Def>
     )
